be17ad3e6de298217021b097dc6866e5d9257ff1
mirsella <mirsella@protonmail.com>
2022-03-23 23:05:06 +0100

    test.js don't return 0 when test fail

--- bin/test.js
+++ bin/test.js
@@ -1,13 +1,23 @@
-const { fork } = require('child_process');
+const { spawn } = require('child_process');
 const { join } = require('path');
 
 const DEMO_URL = 'https://demo.index-education.net/pronote/';
 const DEMO_USERNAME = 'demonstration';
 const DEMO_PASSWORD = 'pronotevs';
 
-function test(type)
-{
-    fork(join(__dirname, 'fetch.js'), [DEMO_URL, DEMO_USERNAME, DEMO_PASSWORD, 'none', type], { stdio: 'inherit' });
+function test(type) {
+    const child = spawn(join(__dirname, 'fetch.js'), [DEMO_URL, DEMO_USERNAME, DEMO_PASSWORD, 'none', type]);
+
+    child.stdout.on('data', data => {
+        // eslint-disable-next-line no-console
+        console.log(`stdout: ${data}`);
+    });
+
+    child.stderr.on('data', data => {
+        if (data.toString().match(/error/ui)) {
+            throw new Error(data);
+        }
+    });
 }
 
 ['student', 'parent'].forEach(test);
